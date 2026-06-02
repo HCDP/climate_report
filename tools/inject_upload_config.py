2674b1b4c5e0bb4150ab8aaebd7fe5ac1cfb5729
jardmclan <mcleanj@hawaii.edu>
2026-06-01 16:31:00 -1000

    remove tapis upload config injection

--- tools/inject_upload_config.py
+++ tools/inject_upload_config.py
@@ -1,4 +1,4 @@
-import json, sys, os
+import json, sys
 from datetime import datetime
 
 config_file = sys.argv[1]
@@ -8,26 +8,6 @@ config = None
 # Load the config file specified as the first command line argument
 with open(config_file, "r") as f:
     config = json.load(f)
-
-# Inject public access permissions
-
-# Default permission setup for uploading data for public access.
-file_permissions = [
-    {
-        "permission": "READ",
-        "recursive": True,
-        "username": "public"
-    }
-]
-# inject file_permissions
-for upload in config["upload"]:
-    upload["file_permissions"] = file_permissions
-
-# Inject auth variables from env
-config["agave_options"]["username"] = os.environ["IW_USERNAME"]
-config["agave_options"]["password"] = os.environ["IW_PASSWORD"]
-config["agave_options"]["api_key"] = os.environ["IW_API_KEY"]
-config["agave_options"]["api_secret"] = os.environ["IW_API_SECRET"]
 
 inject_date = datetime.fromisoformat(date_s)
 config_s = json.dumps(config, indent = 4)
